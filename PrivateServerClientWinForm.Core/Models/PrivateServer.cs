3ff81c2850f28c3f88397fa958173bb09f56374e
Puk06 <86549420+puk06@users.noreply.github.com>
2025-09-08 14:27:40 +0900

    コードの書き方の統一

--- PrivateServerClientWinForm.Core/Models/PrivateServer.cs
+++ PrivateServerClientWinForm.Core/Models/PrivateServer.cs
@@ -1,11 +1,10 @@
-﻿namespace PrivateServerClient.Core.Models
-{
-    public class PrivateServer(string serverEndpoint)
-    {
-        public string ServerAddress { get; private set; } = serverEndpoint;
-        public string ServerApiAddress => $"https://api.{ServerAddress}";
+﻿namespace PrivateServerClient.Core.Models;
 
-        public string ServerApiAddressV1 => $"{ServerApiAddress}/v1";
-        public string ServerApiAddressV2 => $"{ServerApiAddress}/v2";
-    }
+public class PrivateServer(string serverEndpoint)
+{
+    public string ServerAddress { get; private set; } = serverEndpoint;
+    public string ServerApiAddress => $"https://api.{ServerAddress}";
+
+    public string ServerApiAddressV1 => $"{ServerApiAddress}/v1";
+    public string ServerApiAddressV2 => $"{ServerApiAddress}/v2";
 }
