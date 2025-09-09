e8ad9feb31a4bf9bfde6702e554d9c305787e620
Puk06 <86549420+puk06@users.noreply.github.com>
2025-09-09 11:05:41 +0900

    %の位置が間違っていたのを修正

--- PrivateServerClient.WinForm/SubForms/ScoreInfoForm.cs
+++ PrivateServerClient.WinForm/SubForms/ScoreInfoForm.cs
@@ -15,7 +15,7 @@ public partial class ScoreInfoForm : Form
         songNameLabel.Text = score.Beatmap.Title;
         starsLabel.Text = $"★{score.Beatmap.Difficulty:F2}  [{score.Beatmap.Version}]  作者 {score.Beatmap.Creator}";
 
-        scoreStatsLabel.Text = $"{score.Accuracy:F2}\n{score.MaxCombo}%\n{score.Count300}\n{score.Count100}\n{score.Count50}\n{score.Misses}";
+        scoreStatsLabel.Text = $"{score.Accuracy:F2}%\n{score.MaxCombo}\n{score.Count300}\n{score.Count100}\n{score.Count50}\n{score.Misses}";
         ppLabel.Text = $"{score.PerformancePoints}pp";
 
         Text = $"Score Info for ID: {score.Id}";
